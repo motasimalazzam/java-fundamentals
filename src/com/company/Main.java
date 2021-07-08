f95b81c722c96dd14b72f8ad6bb20ff2f9cda85b
motasimalazzam <motasim.h.alazzam@gmail.com>
2021-07-08 20:33:02 +0300

    I corrected the errors according to the feedback I received

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Random;
 
 public class Main {
@@ -55,21 +56,25 @@ public class Main {
 
     }
 
+//    Rolling Dice method
     public static ArrayList roll(int n) {
+
         Random random = new Random();
         int randomNum;
-//        int dice [] = new int [n];
         ArrayList<Integer> dice = new ArrayList<>();
+
         for (int i = 0; i < n; i++) {
             randomNum = random.nextInt(6) + 1;
             dice.add(randomNum);
         }
-//        System.out.println(dice);
         return dice;
     }
 
+//    Contains Duplicates method
     public static boolean containsDuplicates(int[] array) {
-        boolean condition = true;
+
+        boolean condition = false;
+
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array.length; j++) {
                 i++;
@@ -77,7 +82,7 @@ public class Main {
                     break;
                 }
                 if (array[j] == array[i]) {
-                    condition = false;
+                    condition = true;
 
                 }
 
@@ -87,17 +92,25 @@ public class Main {
 
     }
 
-    public static float Averages(int array[]) {
-        int sum = 0;
+//    Calculating Averages method
+    public static double Averages(int array[]) {
+
+        double sum = 0.0 ;
+        double avg;
+
         for (int i = 0; i < array.length; i++) {
             sum = sum + array[i];
         }
-        return sum / array.length;
+        avg = sum / array.length;
+        return avg;
     }
 
-    public static double lowestAverageOfArrays(int arrays[][]) {
+//    Lowest average of arrays
+    public static int[] lowestAverageOfArrays(int arrays[][]) {
+
         int sum1 = 0, sum2 = 0;
         double avg1 = 0.00, avg2 = 0.00;
+        int minAvgArray [] = arrays[0];
 
         for (int i = 0; i < 1; i++) {
             for (int j = 0; j < arrays[i].length; j++) {
@@ -109,14 +122,14 @@ public class Main {
             for (int j = 0; j < arrays[i].length; j++) {
                 sum2 = sum2 + arrays[i][j];
                 avg2 = sum2 / arrays[i].length;
-//                System.out.println("sum2 " + i + " " + j + " " + sum2);
             }
-            if (avg2 > avg1) {
-                avg2 = avg1;
+            if (avg2 < avg1) {
+                avg1 = avg2;
+                minAvgArray = arrays[i];
             }
             sum2 = 0;
         }
-        return avg2;
+        return minAvgArray;
     }
 
 
@@ -131,22 +144,41 @@ public class Main {
         int turtleCount = 0;
         System.out.println("I own " + turtleCount + " " + pluralize("turtle", turtleCount) + ".");
 
-        flipNHeads(5);
+//        flipNHeads(5);
 
-        clock();
+//        clock();
+
+//        CALL AND PRINT Rolling Dice METHOD
         System.out.println(roll(5));
-        int a[] = {5, 5, 4, 3, 2};
-        System.out.println(containsDuplicates(a));
-        System.out.println(Averages(a));
+
+
+        int array1[] = {5, 5, 4, 3, 2};
+        int array2[] = {6, 7, 8, 9, 10, 11};
+//        CALL AND PRINT Contains Duplicates METHODS
+        System.out.println("CALL AND PRINT Contains Duplicates METHODS");
+        System.out.println("Is it contains duplicates value? ---> " + containsDuplicates(array1));
+        System.out.println("Is it contains duplicates value? ---> " + containsDuplicates(array2));
+        System.out.println();
+
+//        CALL AND PRINT AVERAGE METHODS
+        System.out.println("CALL AND PRINT AVERAGE METHODS");
+        System.out.println("The average of array is: " + Averages(array1));
+        System.out.println("The average of array is: " + Averages(array2));
+        System.out.println();
+
+//        CALL AND PRINT LOWEST AVERAGE OF ARRAYS
         int[][] weeklyMonthTemperatures = {
                 {66, 64, 58, 65, 71, 57, 60},
                 {57, 65, 65, 70, 72, 65, 51},
                 {55, 54, 60, 53, 59, 57, 61},
                 {65, 56, 55, 52, 55, 62, 57}
         };
-        System.out.println(lowestAverageOfArrays(weeklyMonthTemperatures));
 
-// test
+        int avgArray [] = lowestAverageOfArrays(weeklyMonthTemperatures);
+
+        System.out.println(" CALL AND PRINT LOWEST AVERAGE OF ARRAYS");
+        System.out.println("LOWEST AVERAGE OF ARRAYS is: " + Arrays.toString(avgArray));
+
     }
 
 
