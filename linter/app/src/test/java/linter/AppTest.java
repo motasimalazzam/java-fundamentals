0e4d43741aff36bc00bda0139edf86c6c65bd622
Motas <motasim.h.alazzam@gmail.com>
2021-07-09 22:35:33 +0300

    finish the second part of the lab

--- linter/app/src/test/java/linter/AppTest.java
+++ linter/app/src/test/java/linter/AppTest.java
@@ -4,11 +4,38 @@
 package linter;
 
 import org.junit.Test;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import static org.junit.Assert.*;
 
 public class AppTest {
-    @Test public void appHasAGreeting() {
-        App classUnderTest = new App();
-        assertNotNull("app should have a greeting", classUnderTest.getGreeting());
+    @Test
+    public void testEmpty() throws IOException {
+        Path path = Paths.get("C:\\Users\\Motas\\IdeaProjects\\java-fundamentals\\linter\\app\\src\\test\\resources\\empty.js");
+        assertEquals(0, App.linterMessage(path.toString()));
+    }
+    @Test
+    public void testNoError() throws IOException {
+        Path path = Paths.get("C:\\Users\\Motas\\IdeaProjects\\java-fundamentals\\linter\\app\\src\\test\\resources\\no-errors.js");
+        assertEquals(0, App.linterMessage(path.toString()));
+    }
+    @Test
+    public void testOneError() throws IOException {
+        Path path = Paths.get("C:\\Users\\Motas\\IdeaProjects\\java-fundamentals\\linter\\app\\src\\test\\resources\\one-error.js");
+        assertEquals(1, App.linterMessage(path.toString()));
+    }
+    @Test
+    public void testThreeErrors() throws IOException {
+        Path path = Paths.get("C:\\Users\\Motas\\IdeaProjects\\java-fundamentals\\linter\\app\\src\\test\\resources\\three-errors.js");
+        assertEquals(3, App.linterMessage(path.toString()));
+    }
+    @Test
+    public void testManyError() throws IOException {
+        Path path = Paths.get("C:\\Users\\Motas\\IdeaProjects\\java-fundamentals\\linter\\app\\src\\test\\resources\\many-errors.js");
+        assertEquals(4, App.linterMessage(path.toString()));
     }
 }
+
